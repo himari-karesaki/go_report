c55a3187dd85bafeb2f6ce9162ff04305bb09c20
himari-karesaki <himari_karesaki@schoo.jp>
2024-07-16 11:13:05 +0900

    修正しました

--- main.go
+++ main.go
@@ -9,22 +9,12 @@ type Item struct {
 }
 
 func main() {
-	fmt.Println("Hello, World!")
-	//品物を入れる変数の定義
-	var category string
-
-	//値段を入れる変数の定義
-	var price int
-
-	fmt.Print("品物を入力してください: ")
-	fmt.Scan(&category)
-
-	fmt.Print("値段を入力してください: ")
-	fmt.Scan(&price)
+	//inputItem関数を呼び出してItem構造体の値を取得
+	item := inputItem()
 
 	fmt.Println("=============")
 
-	fmt.Printf("%sの値段は%d円です\n", category, price)
+	fmt.Printf("%sの値段は%d円です\n", item.Category, item.Price)
 
 	fmt.Println("=============")
 }
