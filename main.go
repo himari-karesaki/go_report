8c9e676ccd87211216be1831b0172b48e573a87e
himari-karesaki <himari_karesaki@schoo.jp>
2024-07-16 11:03:17 +0900

    修正しました

--- main.go
+++ main.go
@@ -2,6 +2,12 @@ package main
 
 import "fmt"
 
+// Item構造体の定義
+type Item struct {
+	Category string
+	Price    int
+}
+
 func main() {
 	fmt.Println("Hello, World!")
 	//品物を入れる変数の定義
@@ -21,4 +27,13 @@ func main() {
 	fmt.Printf("%sの値段は%d円です\n", category, price)
 
 	fmt.Println("=============")
+}
+
+func inputItem() Item {
+	var item Item
+	fmt.Print("品物を入力してください: ")
+	fmt.Scan(&item.Category)
+	fmt.Print("値段を入力してください: ")
+	fmt.Scan(&item.Price)
+	return item
 }
