8037cf10251109e135895ead9ee6aedbe3fa08f2
himari-karesaki <himari_karesaki@schoo.jp>
2024-07-16 12:33:33 +0900

    修正しました

--- main.go
+++ main.go
@@ -9,21 +9,47 @@ type Item struct {
 }
 
 func main() {
-	//inputItem関数を呼び出してItem構造体の値を取得
-	item := inputItem()
 
-	fmt.Println("=============")
+	var n int
+	fmt.Print("何個の品物を登録しますか: ")
+	fmt.Scan(&n)
+	//Item構造体のスライスを作成
+	items := []Item{}
 
-	fmt.Printf("%sの値段は%d円です\n", item.Category, item.Price)
-
-	fmt.Println("=============")
+	for i := 0; i < n; i++ {
+		items = inputItem(items)
+	}
+	showItems(items)
+	// showItems(items)
+	// fmt.Println("===========")
+	// for i := 0; i < len(items); i++ {
+	// 	fmt.Println(items[i].Category, ":", items[i].Price, "円")
+	// }
+	// fmt.Println("===========")
 }
 
-func inputItem() Item {
+func inputItem(items []Item) []Item {
 	var item Item
+
 	fmt.Print("品物を入力してください: ")
 	fmt.Scan(&item.Category)
 	fmt.Print("値段を入力してください: ")
 	fmt.Scan(&item.Price)
-	return item
+	items = append(items, item)
+	fmt.Println(items)
+	return items
+}
+
+func showItems(items []Item) {
+	fmt.Println("===========")
+
+	// itemsの長さだけforを回す
+	// len(items)はitemsの長さを返す
+	for i := 0; i < len(items); i++ {
+
+		fmt.Println(items[i].Category, ":", items[i].Price, "円")
+
+	}
+
+	fmt.Println("===========")
 }
