6945bbdb94ff66a382dbb859ccd9db457730ea79
himari-karesaki <himari_karesaki@schoo.jp>
2024-07-16 17:42:50 +0900

    DBに登録されるように修正しました

--- step2/main.go
+++ step2/main.go
@@ -1,70 +1,47 @@
-// todo: データベースへの記録
-
 package main
 
 import (
-	"database/sql"
 	"fmt"
-	"log"
-
-	"gorm.io/driver/mysql"
-	"gorm.io/gen"
+	"time"
 
 	_ "github.com/go-sql-driver/mysql"
+
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
 )
 
-// Item構造体の定義
 type Item struct {
-	Category string `json:"category" gorm:"primaryKey"`
-	Price    int    `json:"price"`
+	ID        uint `gorm:"primaryKey"`
+	Category  string
+	Price     int
+	CreatedAt time.Time
 }
 
-// posts スライスを宣言
-var items []Item
-
-var db *gorm.DB
-
 func main() {
-
 	// データベースに接続
 	dsn := "root:_MySQLPassword1@tcp(localhost:3306)/bookstore?charset=utf8mb4&parseTime=True&loc=Local"
-	var err error
-	db, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
-		log.Fatal("データベースへの接続に失敗しました:", err)
+		fmt.Println("データベースへの接続に失敗しました:", err)
 	}
 
-	// マイグレーションを実行
-	db.AutoMigrate(&Item{})
-	var category string
-
-	fmt.Print("品目を入力してください：")
-	fmt.Scan(&category)
-
-	// TODO:
-	// データベースへ接続
-	db, err := sql.Open("mysql", "root:_MySQLPassword1@tcp(localhost:3306)/bookstore?charset=utf8mb4&parseTime=true&loc=Local")
+	// テーブルを自動的に作成
+	err = db.AutoMigrate(&Item{})
 	if err != nil {
-		fmt.Println("データベースへの接続に失敗しました：", err)
-		return
-	}
-	defer db.Close()
-
-	// クエリを実行
-	var dbCategory string = category
-
-	var item Item
-
-	err = db.QueryRow("SELECT * FROM items WHERE title = ?", dbCategory).Scan(&item.Category, &item.Price)
-	if err != nil {
-		if err == sql.ErrNoRows {
-			fmt.Println("指定されたタイトルのレコードが見つかりませんでした")
-		} else {
-			fmt.Println("クエリの実行に失敗しました:", err)
-		}
-		return
+		fmt.Println("データベースのマイグレーションに失敗しました:", err)
 	}
 
-	// 結果を出力
-	fmt.Println(item.Category, item.Price)
+	// レコードを挿入
+	items := []Item{
+		{Category: "ピザ", Price: 2980},
+		{Category: "焼肉セット", Price: 1980},
+		{Category: "人参", Price: 1000},
+		{Category: "白菜鍋", Price: 1000},
+	}
+	result := db.Create(&items)
+	if result.Error != nil {
+		fmt.Println("レコードの挿入に失敗しました")
+	}
+	// fmt.Println(result)
+	fmt.Printf("%d レコード挿入しました。\n", result.RowsAffected)
 }
