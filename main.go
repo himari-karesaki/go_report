f7b7ae65ac63038240d0448888b3c899a7865952
himari-karesaki <himari_karesaki@schoo.jp>
2024-07-16 10:55:22 +0900

    変更しました

--- main.go
+++ main.go
@@ -4,4 +4,21 @@ import "fmt"
 
 func main() {
 	fmt.Println("Hello, World!")
+	//品物を入れる変数の定義
+	var category string
+
+	//値段を入れる変数の定義
+	var price int
+
+	fmt.Print("品物を入力してください: ")
+	fmt.Scan(&category)
+
+	fmt.Print("値段を入力してください: ")
+	fmt.Scan(&price)
+
+	fmt.Println("=============")
+
+	fmt.Printf("%sの値段は%d円です\n", category, price)
+
+	fmt.Println("=============")
 }
